9572ebc1fde6fa9083615b956dbe976f3ec7ea24
nicolas asparria <nasparria@gmail.com>
2022-07-19 18:20:50 -0500

    handling errors

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -52,10 +52,21 @@ window.addEventListener('DOMContentLoaded', async () => {
         }
   
       }
-    } catch (e) {
-      console.error("error");
+    } catch (message) {
+      var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+      function alert(message, type) {
+        var wrapper = document.createElement('div')
+        wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+        alertPlaceholder.append(wrapper)
+      }
+
+      if (message) {
+        alert(message, 'warning')
+      }
+      
     }
-  
   });
   
 
