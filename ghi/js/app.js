84f1b4ce27ac6073b51dd24800c5295a16c6a71a
nicolas asparria <nasparria@gmail.com>
2022-07-19 17:49:58 -0500

    putting dates

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,3 +1,20 @@
+function createCard(name, description, pictureUrl, newStartDate, newEndDate) {
+  return `
+    <div class="col">
+      <div class="shadow mb-5 bg-body card rounded">
+        <img src="${pictureUrl}" class="card-img-top"> 
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">${description}</p>
+        </div>
+        <div class="card-footer text-muted">
+          <small class="text-muted"> ${newStartDate} - ${newEndDate} </small>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
 window.addEventListener('DOMContentLoaded', async () => {
 
     const url = 'http://localhost:8000/api/conferences/';
@@ -10,24 +27,33 @@ window.addEventListener('DOMContentLoaded', async () => {
       } else {
         const data = await response.json();
 
-      const conference = data.conferences[0];
-      const nameTag = document.querySelector('.card-title');
-      nameTag.innerHTML = conference.name;
+        for (let conference of data.conferences) {
+          const detailUrl = `http://localhost:8000${conference.href}`;
+          const detailResponse = await fetch(detailUrl);
+          if (detailResponse.ok) {
+            const details = await detailResponse.json();
+            const title = details.conference.name;
+            const description = details.conference.description;
+            const pictureUrl = details.conference.location.picture_url;
+            
+            let startDate = details.conference.starts;
+            let d1 = new Date(startDate)
+            let newStartDate = (d1.getMonth()+1) + "/" + d1.getDate() + "/" + d1.getFullYear()
+            let endDate = details.conference.ends;
+            let ld = new Date(endDate)
+            let newEndDate = (ld.getMonth()+1) + "/" + ld.getDate() + "/" + ld.getFullYear()
 
-      const detailUrl = `http://localhost:8000${conference.href}`;
-      const detailResponse = await fetch(detailUrl);
-      if (detailResponse.ok) {
-        const details = await detailResponse.json();
-        console.log(details);
-      const detailTag = document.querySelector('.card-text');
-      detailTag.innerHTML = details.conference.description;
-      const imageTag = document.querySelector('.card-img-top');
-      imageTag.src = details.conference.location.picture_url;
+            const html = createCard(title, description, pictureUrl, newStartDate, newEndDate);
+            const column = document.querySelector('.row');
+            column.innerHTML += html;
+          }
+        }
+  
       }
-    }
     } catch (e) {
-        console.error("errrrror");
+      console.error("error");
     }
+  
   });
   
 
