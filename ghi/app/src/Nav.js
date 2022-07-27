26ecc16b06c9cd118d3501f363a89df5f7862d23
nicolas asparria <nasparria@gmail.com>
2022-07-26 20:15:58 -0500

    finished app

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -7,7 +7,7 @@ function Nav() {
     <header>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="#">Conference GO!</NavLink>
+        <NavLink className="navbar-brand" to="/">Conference GO!</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
